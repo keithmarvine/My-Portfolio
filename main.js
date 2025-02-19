5086a95a4900f89d7c8cac9be297ba6771a2a524
Keith Marvin <138390683+keithmarvine@users.noreply.github.com>
2025-02-19 10:19:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Show Menu by clicking the mobile icon
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
